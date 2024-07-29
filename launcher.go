7861a6db508187015f9686a6beb684d60ced418f
Eric Anderson <anderson.eric.t@gmail.com>
2024-07-28 22:02:24 -0400

    added alpha logging

--- launcher.go
+++ launcher.go
@@ -1,26 +1,51 @@
 package main
 
 import (
-	routes "goWebServer/routes"
-	server "goWebServer/utility/server"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
-	"path/filepath"
-	"strings"
-	"time"
+
+	routes "goWebServer/routes"
+	server "goWebServer/utility/server"
 
 	"github.com/fsnotify/fsnotify"
 )
 
 func main() {
+	// Create a log file
+	logFile, err := os.OpenFile("http_server.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal("Failed to open log file:", err)
+	}
+	defer logFile.Close()
+
+	// Set up logging to both standard output and the log file
+	log.SetOutput(logFile)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+
+	log.Println("Initializing routes...")
 	routes.HandleRoutes()
+
+	log.Println("Setting up static file server...")
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	server.Serve()
+
+	log.Println("Starting server...")
+	go func() {
+		if len(os.Args) < 2 {
+			log.Fatal("Usage: go run webserver.go <port>")
+		}
+		port := os.Args[1]
+
+		err := server.Serve(port)
+		if err != nil {
+			log.Fatalf("Server error: %v", err)
+		}
+	}()
+
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error creating file watcher:", err)
 	}
 	defer watcher.Close()
 
@@ -36,55 +61,50 @@ func main() {
 					return
 				}
 				if event.Op&fsnotify.Write == fsnotify.Write {
-					log.Println("modified file:", event.Name)
+					log.Println("Modified file:", event.Name)
 					restartChan <- true
 				}
 			case err, ok := <-watcher.Errors:
 				if !ok {
 					return
 				}
-				log.Println("error:", err)
+				log.Println("Watcher error:", err)
 			}
 		}
 	}()
 
-	// Watch current directory and its subdirectories
-	if err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if info.IsDir() && !strings.HasPrefix(info.Name(), ".") {
-			return watcher.Add(path)
-		}
-		return nil
-	}); err != nil {
-		log.Fatal(err)
+	log.Println("Watching for file changes in the 'templates' directory...")
+	err = watcher.Add("templates")
+	if err != nil {
+		log.Fatal("Error adding watcher to templates directory:", err)
 	}
 
 	<-done
 }
 
 func startServer(restartChan chan bool) {
-	cmd := exec.Command("go", "run", "main.go")
-
 	for {
-		log.Println("Starting server...")
-		if err := cmd.Start(); err != nil {
-			log.Fatal(err)
-		}
-		go func() {
-			if err := cmd.Wait(); err != nil {
-				log.Println("Server exited with error:", err)
-			}
-		}()
-
 		<-restartChan
 		log.Println("Restarting server...")
-		if err := cmd.Process.Kill(); err != nil {
-			log.Fatal("Failed to kill process:", err)
+
+		cmd := exec.Command("go", "build", "-o", "server", ".")
+		err := cmd.Run()
+		if err != nil {
+			log.Fatalf("Failed to build server: %v", err)
 		}
 
-		cmd = exec.Command("go", "run", "main.go")
-		time.Sleep(1 * time.Second) // Give some time for the process to release resources
+		cmd = exec.Command("./server")
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		err = cmd.Start()
+		if err != nil {
+			log.Fatalf("Failed to start server: %v", err)
+		}
+
+		err = cmd.Wait()
+		if err != nil {
+			log.Printf("Server exited with error: %v", err)
+		}
 	}
 }
--- utility/server/bootServer.go
+++ utility/server/bootServer.go
@@ -3,18 +3,13 @@ package server
 import (
 	"fmt"
 	"net/http"
-	"os"
 )
 
-func Serve() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: go run webserver.go <port>")
-		return
-	}
-	port := os.Args[1]
+func Serve(port string) error {
 	fmt.Println("Starting server on port:", port)
 	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 	}
+	return nil
 }
